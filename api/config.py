d410a5ced3a434cf5dfae52677e40f1fa8e4b0c8
magic3xx <darkmagic3xx@gmail.com>
2024-11-17 15:30:28 +0200

    Update config.py

--- api/config.py
+++ api/config.py
@@ -1,6 +1,9 @@
 import os
+from pathlib import Path
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///licenses.db'
+    # Use an absolute path for SQLite database in the /tmp directory for serverless environment
+    BASE_DIR = Path('/tmp')
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{BASE_DIR}/licenses.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key-please-change-in-production')
